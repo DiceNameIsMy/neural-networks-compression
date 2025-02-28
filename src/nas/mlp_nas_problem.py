c6d814f69efabdfed75938927507d22e1f2ccb41
Nurdaulet Turar <nurdaulet.turar.cz@gmail.com>
2025-02-28 10:45:37 +0100

    Add NAS code

---
+++ src/nas/mlp_nas_problem.py
@@ -0,0 +1,250 @@
+from dataclasses import dataclass
+
+import numpy as np
+from pymoo.core.problem import ElementwiseProblem
+
+from src.constants import EPOCHS
+from src.datasets.vertebral_dataset import VertebralDataset
+from src.models.mlp import ModelParams, evaluate_model
+from src.models.quantization import ActivationFunc, QMode
+
+
+BITWIDTHS_MAPPING = (2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 32)
+LEARNING_RATES_MAPPING = (
+    0.0001,
+    0.0002,
+    0.0005,
+    0.001,
+    0.002,
+    0.005,
+    0.01,
+    0.02,
+    0.05,
+    0.1,
+    0.2,
+)
+
+
+@dataclass
+class NASParams:
+    hidden_height_bounds: tuple = (2, 8)
+    input_bitwidth_bounds: tuple = (0, 11)
+    hidden_bitwidth_bounds: tuple = (0, 11)
+    layers_amount_bounds: tuple = (2, 8)
+    learning_rate_bounds: tuple = (0, 10)
+    quantization_mode_bounds: tuple = (0, 1)
+
+    epochs: int = EPOCHS
+    min_accuracy: float = 50.0
+
+    amount_of_evaluations: int = 1
+    population_size: int = 30
+    population_offspring_count: int = 10
+
+    def get_n_var(self):
+        return 6
+
+    def get_xl(self):
+        return np.array(np.stack(self._as_array(), 1)[0])
+
+    def get_xu(self):
+        return np.array(np.stack(self._as_array(), 1)[1])
+
+    def _as_array(self):
+        return np.array(
+            [
+                self.layers_amount_bounds,
+                self.hidden_height_bounds,
+                self.input_bitwidth_bounds,
+                self.hidden_bitwidth_bounds,
+                self.learning_rate_bounds,
+                self.quantization_mode_bounds,
+            ]
+        )
+
+
+def get_multiplications_approximation(
+    input_size, output_size, layers_amount, hidden_height
+) -> tuple[int, int, int]:
+    hidden_layers = max(layers_amount - 2, 0)
+
+    # If there are no hidden layers, second layer has the output size
+    second_layer_perceptrons = output_size if hidden_layers == 0 else hidden_height
+
+    # If there are no hidden layers, second layer has the input size
+    almost_last_layer_perceptrons = input_size if hidden_layers == 0 else hidden_height
+
+    first_layer_multiplications = input_size * second_layer_perceptrons
+
+    # When there are 0 or 1 hidden layers,
+    # there are no multiplications between hidden layers.
+    hidden_layers_multiplications = (hidden_height * hidden_height) * max(
+        0, (hidden_layers - 1)
+    )
+
+    # last_layer_multiplications = 0 if hidden_layers == 0 else almost_last_layer_perceptrons * output_size
+    last_layer_multiplications = almost_last_layer_perceptrons * output_size
+
+    return (
+        first_layer_multiplications,
+        hidden_layers_multiplications,
+        last_layer_multiplications,
+    )
+
+
+def get_cost_approximation(
+    input_size,
+    output_size,
+    layers_amount,
+    hidden_height,
+    input_bitwidth,
+    hidden_bitwidth,
+):
+    first_second, hidden_hidden, before_last_last = get_multiplications_approximation(
+        input_size, output_size, layers_amount, hidden_height
+    )
+
+    return (
+        (input_bitwidth * first_second)
+        + (hidden_hidden * hidden_bitwidth)
+        + (before_last_last * hidden_bitwidth)
+    )
+
+
+class NASProblem(ElementwiseProblem):
+    params = None
+    dataset = None
+    train_loader = None
+    test_loader = None
+
+    best_accuracy = 0
+
+    def __init__(self, dataset, params: NASParams):
+        self.params = params
+        self.dataset = dataset
+        self.train_loader, self.test_loader = self.dataset.get_dataloaders()
+
+        super().__init__(
+            n_var=params.get_n_var(),
+            n_obj=5,
+            n_ieq_constr=1,
+            xl=params.get_xl(),
+            xu=params.get_xu(),
+            vtype=int,
+        )
+
+    def _evaluate(self, x, out, *args, **kwargs):
+        conf = self._expand_x(x)
+
+        model_params = self.conf_to_model_params(conf)
+        model_perf = evaluate_model(
+            model_params,
+            self.train_loader,
+            self.test_loader,
+            times=self.params.amount_of_evaluations,
+        )
+        accuracy = model_perf["mean"]
+
+        self.best_accuracy = max(self.best_accuracy, accuracy)
+
+        # TODO: We could choose a better metric than just mean accuracy.
+        # For example compute whether model A is statistically better than model B.
+        # NSGA-2 uses a tournament selection, where this metric can be used.
+
+        # Objectives are optimized to get closer to zero
+        # Objective: Higher model accuracy
+        f1 = 100 - accuracy
+
+        # Objective: Less hidden layers
+        f2 = self.val_to_f(conf["layers_amount"], self.params.layers_amount_bounds)
+
+        # Objective: Less perceptrons per layer
+        f3 = self.val_to_f(conf["hidden_height"], self.params.hidden_height_bounds)
+
+        # Objective: Higher hidden layer quantization
+        f4 = self.val_to_f(
+            conf["hidden_bitwidth"], (min(BITWIDTHS_MAPPING), max(BITWIDTHS_MAPPING))
+        )
+
+        # Objective: Higher inputs quantization
+        f5 = self.val_to_f(
+            conf["input_bitwidth"], (min(BITWIDTHS_MAPPING), max(BITWIDTHS_MAPPING))
+        )
+        out["F"] = [f1, f2, f3, f4, f5]
+
+        # Constraints are optimized to be less than zero
+        # Constraint: Accuracy must be higher than .min_accuracy
+        g1 = -(accuracy - self.params.min_accuracy)
+        out["G"] = [g1]
+
+    def _expand_x(self, x):
+        return {
+            "layers_amount": x[0].astype(int),
+            "hidden_height": x[1].astype(int),
+            "input_bitwidth": BITWIDTHS_MAPPING[x[2].astype(int)],
+            "hidden_bitwidth": BITWIDTHS_MAPPING[x[3].astype(int)],
+            "learning_rate": LEARNING_RATES_MAPPING[x[4].astype(int)],
+            "quantization_mode": QMode.DET if x[5].astype(int) == 0 else QMode.STOCH,
+        }
+
+    def _expand_X(self, X: np.ndarray):
+        return {
+            "layers_amount": X[:, 0].astype(int),
+            "hidden_height": X[:, 1].astype(int),
+            "input_bitwidth": np.array(
+                [BITWIDTHS_MAPPING[x] for x in X[:, 2].astype(int)]
+            ),
+            "hidden_bitwidth": np.array(
+                [BITWIDTHS_MAPPING[x] for x in X[:, 3].astype(int)]
+            ),
+            "learning_rate": np.array(
+                [LEARNING_RATES_MAPPING[x] for x in X[:, 4].astype(int)]
+            ),
+            "quantization_mode": np.where(
+                X[:, 5].astype(int) == 10, QMode.DET, QMode.STOCH
+            ),
+        }
+
+    def val_to_f(self, val, bounds):
+        return (float(val) - bounds[0]) * 100 / (bounds[1] - bounds[0])
+
+    def conf_to_model_params(self, conf):
+        return ModelParams(
+            input_size=self.dataset.input_size,
+            input_bitwidth=conf["input_bitwidth"],
+            output_size=self.dataset.output_size,
+            hidden_size=conf["hidden_height"],
+            hidden_bitwidth=conf["hidden_bitwidth"],
+            model_layers=conf["layers_amount"],
+            quantization_mode=conf["quantization_mode"],
+            learning_rate=conf["learning_rate"],
+            activation=ActivationFunc.BINARIZE,
+            epochs=self.params.epochs,
+        )
+
+    def show_metadata(self):
+        print(f"Best accuracy: {self.best_accuracy}")
+
+
+def test_objectives():
+    nas_params = NASParams()
+    p = NASProblem(VertebralDataset, nas_params)
+    print(p.conf_to_model_params(p._expand_x(nas_params.get_xl())))
+    print(p.conf_to_model_params(p._expand_x(nas_params.get_xu())))
+
+    min_out = dict()
+    p_min = nas_params.get_xl()
+    print(p_min)
+    p._evaluate(p_min, min_out)
+    print("Objective values on min bounds: ", min_out["F"])
+    print("NAS params for min bounds: ", p._expand_x(p_min))
+
+    max_out = dict()
+    p_max = nas_params.get_xu()
+    print(p_max)
+    p._evaluate(p_max, max_out)
+    print("Objective values on max bounds: ", max_out["F"])
+    print("NAS params for max bounds: ", p._expand_x(p_max))
+
+
+test_objectives()
---
+++ src/nas/tooling.py
@@ -0,0 +1,196 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from pymoo.core.result import Result
+from pymoo.algorithms.moo.nsga2 import NSGA2
+from pymoo.operators.crossover.sbx import SBX
+from pymoo.operators.repair.rounding import RoundingRepair
+from pymoo.operators.mutation.pm import PM
+from pymoo.operators.sampling.rnd import IntegerRandomSampling
+from pymoo.termination import get_termination
+from pymoo.optimize import minimize
+
+from src.constants import SEED
+from src.datasets.dataset import Dataset
+from src.models.mlp import ModelParams, evaluate_model
+from src.models.quantization import ActivationFunc
+from src.nas.mlp_nas_problem import NASParams, NASProblem, get_cost_approximation
+
+
+def get_hardware_cost_approximation(df: pd.DataFrame, dataset):
+    data = list()
+    for _, row in df.iterrows():
+        data.append(
+            get_cost_approximation(
+                dataset.input_size,
+                dataset.output_size,
+                row["layers_amount"],
+                row["hidden_height"],
+                row["input_bitwidth"],
+                row["hidden_bitwidth"],
+            )
+        )
+
+    return data
+
+
+def get_pareto_points(x, y):
+    indexes = np.arange(0, len(x))
+    pareto_front = []
+    # Sort by higest accuracy first. If points have same accuracy, put the higher cost first
+    for point in sorted(
+        np.column_stack(((x, y, indexes))), key=lambda v: (-v[0], -v[1])
+    ):
+        if len(pareto_front) == 0:
+            pareto_front.append(point)
+            continue
+
+        last = pareto_front[-1]
+        if point[1] <= last[1]:
+            pareto_front.append(point)
+
+    return pareto_front
+
+
+def plot_pareto_front(accuracy, cost, pareto_front):
+    # Plot the data
+    plt.figure(figsize=(10, 6))
+    plt.scatter(accuracy, cost, facecolors="none", edgecolors="blue")
+    plt.plot(
+        pareto_front[:, 0],
+        pareto_front[:, 1],
+        color="red",
+        label="Pareto Frontier",
+        linewidth=2,
+    )
+
+    # Beautify the plot
+    max_cost = max(cost)
+    plt.ylim(max_cost * 1.1, -(max_cost * 0.1))
+
+    accuracy_range = max(accuracy) - min(accuracy)
+    plt.xlim(
+        min(accuracy) - (accuracy_range * 0.1), max(accuracy) + (accuracy_range * 0.1)
+    )
+
+    plt.title("Pareto Front")
+    plt.ylabel("Cost")
+    plt.xlabel("Accuracy")
+    plt.grid(True)
+    plt.show()
+
+
+def get_population_df(problem, res: Result, plot=True):
+    # Create a dataframe of final population
+    population_conf = problem._expand_X(res.X)
+    df = pd.DataFrame(population_conf)
+
+    accuracy = 100 - res.F[:, 0]
+    df["accuracy"] = accuracy
+
+    cost = get_hardware_cost_approximation(df, problem.dataset)
+    df["cost"] = cost
+
+    # Compute & Plot pareto front
+
+    pareto_front = np.array(get_pareto_points(df["accuracy"], df["cost"]))
+
+    if plot:
+        plot_pareto_front(accuracy, cost, pareto_front)
+
+    pareto_idxs = pareto_front[:, 2]
+    pf = df.iloc[pareto_idxs.astype(int)]
+    return {"all": df, "pareto": pf}
+
+
+def train_pf(pf, DatasetClass, epochs):
+    train_loader, test_loader = DatasetClass.get_dataloaders()
+
+    def _train_conf(conf):
+        params = ModelParams(
+            input_size=DatasetClass.input_size,
+            input_bitwidth=conf["input_bitwidth"],
+            output_size=DatasetClass.output_size,
+            hidden_size=conf["hidden_height"],
+            hidden_bitwidth=conf["hidden_bitwidth"],
+            model_layers=conf["layers_amount"],
+            learning_rate=conf["learning_rate"],
+            activation=ActivationFunc.BINARIZE,
+            epochs=epochs,
+            quantization_mode=conf["quantization_mode"],
+        )
+        perf = evaluate_model(params, train_loader, test_loader, times=5)
+        conf["best"] = perf["max"]
+        conf["mean"] = perf["mean"]
+        conf["std"] = perf["std"]
+        return conf
+
+    return pf.apply(_train_conf, axis=1)
+
+
+def run_NAS_pipeline(
+    DatasetClass: Dataset, params: NASParams, n_gen=1, train_pf_epochs=10
+):
+    problem = NASProblem(DatasetClass, params)
+
+    algorithm = NSGA2(
+        pop_size=params.population_size,
+        n_offsprings=params.population_offspring_count,
+        sampling=IntegerRandomSampling(),
+        crossover=SBX(prob=0.9, eta=15, repair=RoundingRepair()),
+        mutation=PM(eta=20, repair=RoundingRepair()),
+    )
+
+    termination = get_termination("n_gen", n_gen)
+    res = minimize(
+        problem, algorithm, termination, seed=SEED, save_history=False, verbose=True
+    )
+
+    problem.show_metadata()
+
+    population = get_population_df(problem, res)
+
+    population["pareto_trained"] = train_pf(
+        population["pareto"], DatasetClass, train_pf_epochs
+    )
+    return population
+
+
+def run_simple_grid_search(DatasetClass: Dataset):
+    # A model trained without NAS
+    # TODO: How to properly evaluate my baseline?
+
+    no_NAS_datapoints = []
+    for layers in range(2, 4 + 1):
+        for layer_size in range(4, 17, 4):
+            p = ModelParams(
+                input_size=DatasetClass.input_size,
+                input_bitwidth=8,
+                output_size=DatasetClass.output_size,
+                hidden_size=layer_size,
+                hidden_bitwidth=8,
+                model_layers=layers,
+                learning_rate=0.01,
+                activation=ActivationFunc.BINARIZE,
+                epochs=20,
+            )
+            train_loader, test_loader = DatasetClass.get_dataloaders()
+
+            accuracy = evaluate_model(p, train_loader, test_loader, times=3)
+            cost = get_cost_approximation(
+                p.input_size,
+                p.output_size,
+                p.model_layers,
+                p.hidden_size,
+                p.input_bitwidth,
+                p.hidden_bitwidth,
+            )
+            no_NAS_datapoints.append([layers, layer_size, accuracy, cost])
+
+    best_accuracy = max(no_NAS_datapoints, key=lambda x: x[2])
+
+    print(
+        f"Simple grid search results: Layers: {best_accuracy[0]}, Hidden layers size: {best_accuracy[1]} ",
+        f"Accuracy: {best_accuracy[2]}, Cost: {best_accuracy[3]}.",
+    )
