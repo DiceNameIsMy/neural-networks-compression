1814b120195b4648f0fe49bae0a06b6514f6929a
Nurdaulet Turar <nurdaulet.turar.cz@gmail.com>
2025-03-08 18:37:19 +0100

    fix: Proper caching folder

--- src/datasets/mnist_dataset.py
+++ src/datasets/mnist_dataset.py
@@ -17,10 +17,10 @@ def fetch_mnist_dataset():
 
     # Load datasets
     train_dataset = torchvision.datasets.MNIST(
-        root="./data", train=True, download=True, transform=transform
+        root="./datasets_cache/", train=True, download=True, transform=transform
     )
     test_dataset = torchvision.datasets.MNIST(
-        root="./data", train=False, download=True, transform=transform
+        root="./datasets_cache/", train=False, download=True, transform=transform
     )
     return train_dataset, test_dataset
 
