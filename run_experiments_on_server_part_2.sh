93740dafd577eb962a06c0874405e7de80526f6f
Nurdaulet Turar <nurdaulet.turar.cz@gmail.com>
2025-06-02 14:05:03 +0200

    Add scripts to run experiments

---
+++ run_experiments_on_server_part_2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+CUDA_VISIBLE_DEVICES=1 nohup time python src/main.py -l debug nas -d breast-cancer --epochs 30 --patience 5 --evaluations 3 --generations 5 --population 40 --offspring 8 --store-models -P -H -O reports/breast-cancer &
+CUDA_VISIBLE_DEVICES=2 nohup time python src/main.py -l debug nas -d cardio --epochs 30 --patience 5 --evaluations 3 --generations 5 --population 40 --offspring 8 --store-models -P -H -O reports/cardio &
+CUDA_VISIBLE_DEVICES=3 nohup time python src/main.py -l debug experiment1 --epochs 30 --patience 5 --evaluations 3 --size mini &
