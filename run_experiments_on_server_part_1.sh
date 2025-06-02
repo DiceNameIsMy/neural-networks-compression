3ee4c0917981476cdee689899b456240bf457c04
Nurdaulet Turar <nurdaulet.turar.cz@gmail.com>
2025-06-02 17:12:07 +0200

    fix: use mini version of cnn datasets for NAS

--- run_experiments_on_server_part_1.sh
+++ run_experiments_on_server_part_1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 CUDA_VISIBLE_DEVICES=1 nohup time python src/main.py -l debug nas -d vertebral --epochs 30 --patience 5 --evaluations 3 --generations 5 --population 40 --offspring 8 --store-models -P -H -O reports/vertebral &
-CUDA_VISIBLE_DEVICES=2 nohup time python src/main.py -l debug nas -d mnist --epochs 30 --patience 5 --evaluations 1 --generations 1 --population 40 --offspring 8 --store-models -P -H -O reports/mnist &
-CUDA_VISIBLE_DEVICES=3 nohup time python src/main.py -l debug nas -d cifar10 --epochs 30 --patience 5 --evaluations 3 --generations 5 --population 40 --offspring 8 --store-models -P -H -O reports/cifar10 &
+CUDA_VISIBLE_DEVICES=2 nohup time python src/main.py -l debug nas -d mini-mnist --epochs 30 --patience 5 --evaluations 1 --generations 1 --population 40 --offspring 8 --store-models -P -H -O reports/mini-mnist &
+CUDA_VISIBLE_DEVICES=3 nohup time python src/main.py -l debug nas -d mini-cifar10 --epochs 30 --patience 5 --evaluations 3 --generations 5 --population 40 --offspring 8 --store-models -P -H -O reports/mini-cifar10 &
